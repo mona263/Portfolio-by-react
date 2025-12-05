819e8a71bc92a2a06dfc7a7efe5c9d283b912f6b
Mona Abdelazeem <monaabdelazeem263.com>
2025-12-05 17:19:48 +0200

    My React Portfolio

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MotionConfig } from 'framer-motion';
+import './App.css';
+import BackgroundAnimation from './components/BackgroundAnimation';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Services from './components/Services';
+import Certifications from './components/Certifications';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+     <Router>
+    
+    <MotionConfig reducedMotion="user">
+      <div className="App">
+        <BackgroundAnimation />
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+           <Services />
+      <Certifications />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </MotionConfig>
+    </Router>
+  );
+}
+
+export default App;
